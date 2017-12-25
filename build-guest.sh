3ec6b4822cb3c506df0ed5d298cebe8c43d4923c
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2017-12-25 12:16:19 +0900

    Minor modification on build-guest.sh

--- build-guest.sh
+++ build-guest.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+set -e
+
 # Build rumprun in guest environment.
 
+if [ ! -f /etc/vagrant_provisioned ]; then
+    echo "Seems to be in host environment."
+    exit 1
+fi
+
 CC=x86_64-linux-gnu-gcc
-TARGET=x86_64-linux-gnu
 # CC=aarch64-linux-gnu-gcc
-# TARGET=aarch64-linux-gnu
+TARGET=${CC%-gcc}
 
 # sudo DEBIAN_FRONTEND=noninteractive apt -qq install -y \
 #     make \
@@ -17,18 +23,19 @@ TARGET=x86_64-linux-gnu
 #     gcc-aarch64-linux-gnu
 
 mkdir_p() {
-    if [ ! -e $1 ]; then
-        mkdir $1
-    fi
+    for d in $@; do
+        if [ ! -e $d ]; then
+            mkdir $d
+        fi
+    done
 }
 
 rr_build="$HOME/rumprun-build" # can not create hard link in synced dir
 mkdir_p $rr_build
 
 rr_obj="../../rumprun-build/obj-$TARGET" # seems to accept only relative paths
-mkdir_p $rr_obj
 rr_stage="../../rumprun-build/stage-$TARGET"
-mkdir_p $rr_stage
+mkdir_p $rr_obj $rr_stage
 
 CC=$CC ./build-rr.sh -j2 -o $rr_obj -d $rr_stage hw $@
 
