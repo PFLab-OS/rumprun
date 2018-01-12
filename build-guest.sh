fddaca32c3212b07dbf54b6c0907edb12907e2fc
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2018-01-12 12:30:05 +0900

    Remove build-arm.sh and fix build-guest.sh

--- build-guest.sh
+++ build-guest.sh
@@ -4,23 +4,24 @@ set -e
 
 # Build rumprun in guest environment.
 
-if [ ! -f /etc/vagrant_provisioned ]; then
-    echo "Seems to be in host environment."
+if [ ! -d /vagrant ]; then
+    echo "# Error: Seems to be in host environment."
     exit 1
 fi
 
-CC=x86_64-linux-gnu-gcc
+# CC=x86_64-linux-gnu-gcc
 # CC=aarch64-linux-gnu-gcc
+CC=arm-linux-gnueabihf-gcc
 TARGET=${CC%-gcc}
 
 # sudo DEBIAN_FRONTEND=noninteractive apt -qq install -y \
 #     make \
 #     gcc \
-#     g++ \
 #     zlib1g-dev \
 #     qemu-system-x86 \
 #     qemu-system-aarch64 \
-#     gcc-aarch64-linux-gnu
+#     gcc-aarch64-linux-gnu \
+#     gcc-arm-linux-gnueabihf
 
 mkdir_p() {
     for d in $@; do
@@ -30,20 +31,23 @@ mkdir_p() {
     done
 }
 
-rr_build="$HOME/rumprun-build" # can not create hard link in synced dir
+rr_build=../../rumprun-build   # can not create hard link in synced dir
 mkdir_p $rr_build
 
-rr_obj="../../rumprun-build/obj-$TARGET" # seems to accept only relative paths
-rr_stage="../../rumprun-build/stage-$TARGET"
+rr_obj=$rr_build/obj-$TARGET
+rr_stage=$rr_build/stage-$TARGET
 mkdir_p $rr_obj $rr_stage
 
 CC=$CC ./build-rr.sh -j2 -o $rr_obj -d $rr_stage hw $@
 
-cat > guest-bashrc << EOF
-export PATH="\$HOME/rumprun-build/stage-$TARGET/bin:\$PATH"
+rr_bashrc=rr-bashrc
+
+cat > $rr_build/$rr_bashrc << EOF
+export PATH="$(cd $rr_stage; pwd)/bin:\$PATH"
 export RUMPRUN_TOOLCHAIN_TUPLE=${TARGET%%-*}-rumprun-netbsd
 EOF
 
-if ! grep guest-bashrc ~/.bashrc; then
-    echo "source $PWD/guest-bashrc" >> ~/.bashrc
+shrc=$HOME/.${SHELL##*/}rc
+if ! grep $rr_bashrc $shrc; then
+    echo "source $(cd $rr_build; pwd)/$rr_bashrc" >> $shrc
 fi
